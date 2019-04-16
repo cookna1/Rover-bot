873109448f20df82dd6bc94f9b3ce7fd6bce1151
cookna1 <31253513+cookna1@users.noreply.github.com>
2019-04-16 14:20:48 -0400

    Update main.c

--- IR_DETECTOR/IR_DETECTOR/main.c
+++ IR_DETECTOR/IR_DETECTOR/main.c
@@ -9,36 +9,42 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-
+volatile int saved = 3;
 
 int main(void)
 {
 	
+	 
+	DDRF = 0x03;
+	//DDRF = (1 << DDF0) | (1 << DDF1);
+	PCICR = 4;
+	PCMSK2 = (1 << PCINT16) | (1 << PCINT17);
+
 	
-	DDRF |= 0x00;
-	DDRB |= (1 << PB5);
-	
-	PCMSK2 = 0x80;
-	PORTB = 0;
+	//PORTB = 0;
 	sei();
     /* Replace with your application code */
     while (1) 
     {
-		if((PINF & (1 << PF0))) {
-			PORTB |= (1 << PB5);
-		}
-		else
-			PORTB = 0;
+		
     }
 }
 
+//LEFT SENSOR and RIGHT 
+/
 ISR(PCINT2_vect) {
 	
+	if(PINK & (1 << PK0)){ //LEFT SENSOR
+		PORTF |= (1 << PF0);  //TODO This part should change based on what needs to be done if left sensor get high
+		_delay_ms(100);
+		PORTF &= 2;
+	}
+	else if(PINK & (1 << PK1)) { //RIGHT SENSOR
+		PORTF |= (1 << PF1); //TODO This part should change based on what needs to be done if right sensor get high
+		_delay_ms(100);
+		PORTF &= 1;
+	}
 }
-
-
-
-
 
 
 
